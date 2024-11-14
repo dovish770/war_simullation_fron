276cc76b09430dab5c5b2a0c7a71b3c671ab5e31
dovi stein <dovish@gmail.com>
2024-11-14 17:57:13 +0200

    finaly connected to socket

--- frontend/src/socket/socketService.ts
+++ frontend/src/socket/socketService.ts
@@ -1,32 +1,19 @@
-import { io, Socket } from 'socket.io-client';
-import ArsenalResponse from '../types/modelTypes/ArsenalResponse';
-
+import { io, Socket } from "socket.io-client";
+import ArsenalResponse from "../types/modelTypes/ArsenalResponse";
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
+const socket: Socket = io(BASE_URL);  
 
-let socket: Socket | null;
+export function joinSocketRoom(roomName: string): void {
+  socket.emit("joinRoom", roomName);
 
-
-export const getSocketInstance = (): Socket => {
-    if (!socket) {
-        socket = io(BASE_URL, {
-            transports: ['websocket']
-          }); 
-    }
-    return socket;
-};
-
-export const joinRoom = (region: string) => {
-    const socket = getSocketInstance(); 
-    console.log(socket.connected);    
-    socket.emit('join_room', region.toLocaleLowerCase());  
-};
-
+  socket.on("roomStatus", (message: string) => {
+    console.log(message);  
+  });
+}
 
 export const activeOrNotSocket = (arsenal:ArsenalResponse)=>{
     const region:string[] = arsenal.organization.name.split(" ");
     if (region.length>1){
-        console.log("start socket");
-        joinRoom(region[2])
-    }
+        joinSocketRoom(region[2])    }
 }
